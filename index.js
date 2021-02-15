069d7ec7190cdde2d8cd9c3ca68e980f841bd0ec
zzmin13 <zzzz1997@naver.com>
2021-02-15 14:10:40 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const PORT = 4000;
 function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req,res){
+    console.log(req);
+    res.send('Hello from home');
+}
+
+function handleProfile(req,res){
+    res.send("You are on my profile");
+}
+app.get("/", handleHome);
+app.get("/profile",handleProfile);
 app.listen(PORT, handleListening);
