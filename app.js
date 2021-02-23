0d265c7d64c10d6e39d88dec8e9bb9a7ef97013e
zzmin13 <zzzz1997@naver.com>
2021-02-23 18:08:57 +0900

    home controller part 2

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ import { localsMiddleware } from './middlewares';
 const app = express();
 
 app.use(helmet());
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    return next();
+    });
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -24,5 +28,7 @@ app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos,videoRouter);
+
+
 
 export default app;
--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://youtu.be/0-q1KafFCLU",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Minkyoung kang",
@@ -17,7 +17,7 @@ export const videos = [
         title: 'Video super',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://youtu.be/GeeM2V15QFY",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Minkyoung kang",
@@ -30,7 +30,7 @@ export const videos = [
         title: 'Video nice',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://youtu.be/3iM_06QeZi8",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Minkyoung kang",
@@ -43,7 +43,7 @@ export const videos = [
         title: 'Video perfect',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://youtu.be/D1PvIWdJ8xo",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Minkyoung kang",
