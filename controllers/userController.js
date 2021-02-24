e5de9ea6658ffa5d2632110f6e2cce9766d7037e
zzmin13 <zzzz1997@naver.com>
2021-02-24 14:42:02 +0900

    Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,10 @@ export const postJoin = (req,res) =>{
     }
 }
 
-export const login = (req,res) => res.render("login", {pageTitle: "Login"});
+export const getLogin = (req,res) => res.render("login", {pageTitle: "Login"});
+export const postLogin = (req,res) => {
+    res.redirect(routes.home);
+}
 export const logout = (req,res) => res.render("logout", {pageTitle: "Logout"});
 
 export const userDetail = (req,res) => res.render("userDetail", {pageTitle: "User Detail"});
--- routes.js
+++ routes.js
@@ -25,7 +25,13 @@ const routes = {
     logout: LOGOUT,
     search: SEARCH,
     users: USERS,
-    userDetail: USER_DETAIL,
+    userDetail: (id) => {
+        if(id){
+            return `/users/${id}`;
+        }else{
+            return USER_DETAIL;
+        }
+    },
     editProfile: EDIT_PROFILE,
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
