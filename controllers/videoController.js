0825407185f2304ae59b6e6ca8b6acb734275aad
zzmin13 <zzzz1997@naver.com>
2021-02-24 13:55:37 +0900

    Join Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,7 +5,7 @@ export const home = (req, res) => {
 
 export const search = (req, res) => {
     const {query: { term: searchingBy }} = req; // const searchingBy = req.query.term 과 같은 코드
-    res.render("search", {pageTitle: "Search", searchingBy}); // searchingBy = searchingBy 인 경우 하나로 줄여쓸 수 있음
+    res.render("search", {pageTitle: "Search", searchingBy, videos}); // searchingBy = searchingBy 인 경우 하나로 줄여쓸 수 있음
 
 };
 
