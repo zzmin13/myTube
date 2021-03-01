8a7b1942e1f2fb4f17768ae372946f78921e41e4
zzmin13 <zzzz1997@naver.com>
2021-03-01 15:00:51 +0900

    Configuring Dot Env

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-mongoose.connect("mongodb://localhost:27017/mytube",
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
 {
     useNewUrlParser: true,
     useFindAndModify:false
-});
+}
+);
 
 const db = mongoose.connection;
 
