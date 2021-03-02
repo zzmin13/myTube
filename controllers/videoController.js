1992cf8a5c334abe86752a94ac9dfc404e131518
zzmin13 <zzzz1997@naver.com>
2021-03-02 17:13:54 +0900

    Uploading and Creating a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,16 +1,17 @@
 import routes from '../routes';
 import Video from "../models/Video";
 
-export const home = async(req, res) => {
+export const home = async(req, res) => {
     try{
         const videos = await Video.find({});
         res.render("home", {pageTitle: "Home", videos});
-    } catch(error){
+    }catch(error){
         console.log(error);
-        res.render("home", {pageTitle: "Home", videos:[]});
+        res.render("home", {pageTitle: "Home", videos :[]});
     }
-    
+        
 };
+    
 
 export const search = (req, res) => {
     const {query: { term: searchingBy }} = req; // const searchingBy = req.query.term 과 같은 코드
@@ -19,10 +20,19 @@ export const search = (req, res) => {
 };
 
 export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
-export const postUpload = (req, res) => {
-    const{body: {file,title,description}} = req;
+export const postUpload = async(req, res) => {
+    const {
+        body: {title,description},
+        file: {path}
+    } = req;
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    console.log(newVideo);
     //To Do: Upload and save video
-    res.redirect(routes.videoDetail(324393));
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
