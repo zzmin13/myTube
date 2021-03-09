31cd8834f26b47f82ba19dacb04146639171f409
zzmin13 <zzzz1997@naver.com>
2021-03-09 16:26:44 +0900

    Local Authentication with Passport part 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from '../routes';
+import User from "../models/User";
 
 export const getJoin = (req,res) =>{
     res.render("join", {pageTitle: "Join"});
 }
 
-export const postJoin = (req,res) =>{
+export const postJoin = async (req,res) => {
     // console.log(req.body);
     const{
         body:{name, email, password, password2}
@@ -13,7 +14,15 @@ export const postJoin = (req,res) =>{
         res.status(400);
         res.render("join", {pageTitle: "Join"});
     }else{
-        //To Do: Register User
+        try{
+            const user = await User({
+                name,
+                email
+            });
+            await User.register(user,password);
+        }catch(error){
+            console.log(error);
+        }
         //To Do: Log User In
         res.redirect(routes.home);
     }
