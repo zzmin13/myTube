8bf00164eba48dfed056a5a8af1ee1265cc16f49
zzmin13 <zzzz1997@naver.com>
2021-03-15 14:51:25 +0900

    User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -123,5 +123,5 @@ export const userDetail = async (req,res) => {
     }
 }
 
-export const editProfile = (req,res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const getEditProfile = (req,res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req,res) => res.render("changePassword", {pageTitle: "Change Password"});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { changePassword,
         editProfile,
+        getEditProfile,
         userDetail,
         } from '../controllers/userController';
 import { onlyPrivate } from '../middlewares';
 import routes from "../routes";
 
 const userRouter = express.Router();
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
