848083ade8cbffab85034b20b5231e05977f7f3b
zzmin13 <zzzz1997@naver.com>
2021-03-26 14:35:08 +0900

    modified

--- src/init.js
+++ src/init.js
@@ -10,6 +10,7 @@ import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`✅Listening on: http://localhost:${PORT}`);
-
+const handleListening = () => {
+    console.log(`✅Listening on: http://localhost:${PORT}`);
+}
 app.listen(PORT, handleListening);
